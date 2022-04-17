e12fc06d0ca710655d6432e54f6bb2e43cd450ee
Sakhmina <asadullaevasakhmina@gmail.com>
2022-04-17 23:55:36 +0300

    Initial commit

---
+++ src/main/java/person/Person.java
@@ -0,0 +1,74 @@
+package person;
+
+import java.util.Objects;
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    private int age;
+    private String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+
+    public boolean hasAge() {
+        return !OptionalInt.of(age).isEmpty();
+    }
+
+    public boolean hasAddress() {
+        return !address.isEmpty();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return OptionalInt.of(age);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        this.age++;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+
+    // метод для получения полузаполненного билдера для ребёнка
+    public PersonBuilder newChildBuilder() { /*...*/ }
+
+}
